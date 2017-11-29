c4ac4c40e7ef805fa4eb4c8d00c4dcc244a03285
rwilleyii <rwilley@me.com>
2017-11-29 06:04:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 function scuberGreetingForFeet(feet) {
   if (feet <= 400) {
     return 'This one is on me!'
+  } else if (feet > 2000 && feet >= 2500) {
+    return 'I will gladly take your thirty bucks.'
   } else if (feet > 2500) {
     return 'No can do.'
-  } else if (feet > 2000) {
-    return 'I will gladly take your thirty bucks.'
   }
 }
 
