b398ae9a146e0b1eaa26a0fbf0e07e0bcc3c2927
rwilleyii <rwilley@me.com>
2017-11-29 05:53:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return 'This one is on me!'
+  } else if (feet > 2500) {
+    return 'No can do.'
+  } else if (feet > 2000) {
+    return 'I will gladly take your thirty bucks.'
+  }
+}
+
+function ternaryCheckCity(city) {
+  let response;
+  city === 'NYC' ? response = "Ok, sounds good." : response = "No go.";
+  return response
+}
+
+function switchOnCharmFromTip(tip) {
+  let response;
+
+  switch(tip) {
+    case 'generous':
+      response = "Thank you so much.";
+      break;
+    case 'not as generous':
+      response = "Thank you.";
+      break;
+    default:
+      response = "Bye."
+  }
+
+  return response
+}
